0cb1628ac0f51c1a7d5fdb835f4d04b8fd49a950
Cesar Augusto Garcia <cesar.garcia@mulesoft.com>
2013-11-21 19:00:52 -0300

    Code refactor on transformer

--- src/main/java/org/mule/kicks/transformers/SFDCUsersMegeTransformer.java
+++ src/main/java/org/mule/kicks/transformers/SFDCUsersMegeTransformer.java
@@ -2,6 +2,10 @@ package org.mule.kicks.transformers;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
@@ -9,89 +13,82 @@ import org.mule.transformer.AbstractMessageTransformer;
 
 public class SFDCUsersMegeTransformer extends AbstractMessageTransformer {
 
-	private String QUERY_COMPANY_A = "usersFromOrgA";
-	private String QUERY_COMPANY_B = "usersFromOrgB";
-	
-	private ArrayList<HashMap> usersFromOrgAList; 
-	private ArrayList<HashMap> usersFromOrgBList; 
-	
+	private static final String QUERY_COMPANY_A = "usersFromOrgA";
+	private static final String QUERY_COMPANY_B = "usersFromOrgB";
+
 	@Override
-	public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
-		
-		usersFromOrgAList = message.getInvocationProperty(QUERY_COMPANY_A);
-		usersFromOrgBList = message.getInvocationProperty(QUERY_COMPANY_B);
-		ArrayList<HashMap> mergedUsersList = new ArrayList<HashMap>();
-		
-		for (HashMap userFromA : usersFromOrgAList )
-		{
-			HashMap userFromB = getUserFromListB(userFromA.get("Email"));
+	public Object transformMessage(MuleMessage message, String outputEncoding)	throws TransformerException {
+
+		List<Map<String, String>> usersFromOrgAList = message.getInvocationProperty(QUERY_COMPANY_A);
+		List<Map<String, String>> usersFromOrgBList = message.getInvocationProperty(QUERY_COMPANY_B);
+
+		List<Map<String, String>> mergedUsersList = new ArrayList<Map<String, String>>();
+
+		for (Map<String, String> userFromA : usersFromOrgAList) {
 			
-			HashMap newMergedUser = new HashMap();
-			
+			Map<String, String> userFromB = getUserFromListB(userFromA.get("Email"), usersFromOrgBList);
+
+			Map<String, String> newMergedUser = new HashMap<String, String>();
+
 			newMergedUser.put("Name", userFromA.get("Name"));
 			newMergedUser.put("Mail", userFromA.get("Email"));
 			newMergedUser.put("IDInA", userFromA.get("Id"));
 			newMergedUser.put("UserNameInA", userFromA.get("Username"));
-			
+
 			newMergedUser.put("IDInB", userFromB.get("Id"));
 			newMergedUser.put("UserNameInB", userFromB.get("Username"));
-			
+
 			mergedUsersList.add(newMergedUser);
 		}
-		
-		for (HashMap userFromB : usersFromOrgBList )
-		{
-			
-			if (!checkExistingUserInA(userFromB.get("Email")))
-			{
-				HashMap newMergedUser = new HashMap();
-				
+
+		for (Map<String, String> userFromB : usersFromOrgBList) {
+
+			if (!checkExistingUserInA(userFromB.get("Email"), usersFromOrgAList)) {
+				Map<String, String> newMergedUser = new HashMap<String, String>();
+
 				newMergedUser.put("Name", userFromB.get("Name"));
 				newMergedUser.put("Mail", userFromB.get("Email"));
 				newMergedUser.put("IDInA", "");
 				newMergedUser.put("UserNameInA", "");
-				
+
 				newMergedUser.put("IDInB", userFromB.get("Id"));
 				newMergedUser.put("UserNameInB", userFromB.get("Username"));
-				
+
 				mergedUsersList.add(newMergedUser);
 			}
-			
+
 		}
-				
+
 		return mergedUsersList;
 	}
-	
-	private Boolean checkExistingUserInA (Object mailFromUserB)
-	{
-		for (HashMap userFromA : usersFromOrgAList) 
-		{
-			if (userFromA.get("Email").equals(mailFromUserB))
-			{
+
+	private Boolean checkExistingUserInA(Object mailFromUserB, List<Map<String, String>> usersFromOrgAList) {
+		
+		for (Map<String, String> userFromA : usersFromOrgAList) {
+			if (userFromA.get("Email").equals(mailFromUserB)) {
 				return true;
 			}
 		}
 		return false;
+		
 	}
-	
-	private HashMap getUserFromListB (Object mailFromUserA){
-		
-		HashMap resultingUserFromB = new HashMap();
-		
+
+	private Map<String, String> getUserFromListB(Object mailFromUserA, List<Map<String, String>> usersFromOrgBList) {
+
+		Map<String, String> resultingUserFromB = new HashMap<String, String>();
+
 		resultingUserFromB.put("Id", "");
 		resultingUserFromB.put("Username", "");
-		
-		for (HashMap userFromB : usersFromOrgBList) 
-		{
-			if (userFromB.get("Email").equals((String) mailFromUserA))
-			{
+
+		for (Map<String, String> userFromB : usersFromOrgBList) {
+			if (userFromB.get("Email").equals((String) mailFromUserA)) {
 				resultingUserFromB.put("Id", userFromB.get("Id"));
 				resultingUserFromB.put("Username", userFromB.get("Username"));
-				
+
 				return resultingUserFromB;
 			}
 		}
-		
+
 		return resultingUserFromB;
 	}
 
