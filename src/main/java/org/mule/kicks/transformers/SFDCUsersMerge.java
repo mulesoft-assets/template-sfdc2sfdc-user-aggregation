8de086bb44182fabfb1c2bdec3d7b75622d70174
Cesar Augusto Garcia <cesar.garcia@mulesoft.com>
2014-01-21 11:33:48 -0300

    Separating Sort from Mering having two Java Transformers as a result

--- src/main/java/org/mule/kicks/transformers/SFDCUsersMerge.java
+++ src/main/java/org/mule/kicks/transformers/SFDCUsersMerge.java
@@ -25,44 +25,6 @@ public class SFDCUsersMerge extends AbstractMessageTransformer {
 	private static final String QUERY_COMPANY_A = "usersFromOrgA";
 	private static final String QUERY_COMPANY_B = "usersFromOrgB";
 
-	public static Comparator<Map<String, String>> recordComparator = new Comparator<Map<String, String>>() {
-
-		public int compare(Map<String, String> user1, Map<String, String> user2) {
-
-			String key1 = buildKey(user1);
-			String key2 = buildKey(user2);
-
-			return key1.compareTo(key2);
-
-		}
-
-		private String buildKey(Map<String, String> user) {
-			StringBuilder key = new StringBuilder();
-
-			if (StringUtils.isNotBlank(user.get("IDInA")) && StringUtils.isNotBlank(user.get("IDInB"))) {
-				key.append("~~");
-				key.append(user.get("IDInA"));
-				key.append(user.get("IDInB"));
-				key.append(user.get("Email"));
-			}
-
-			if (StringUtils.isNotBlank(user.get("IDInA")) && StringUtils.isBlank(user.get("IDInB"))) {
-				key.append(user.get("IDInA"));
-				key.append("~");
-				key.append(user.get("Email"));
-			}
-
-			if (StringUtils.isBlank(user.get("IDInA")) && StringUtils.isNotBlank(user.get("IDInB"))) {
-				key.append("~");
-				key.append(user.get("IDInB"));
-				key.append(user.get("Email"));
-			}
-
-			return key.toString();
-		}
-
-	};
-
 	@SuppressWarnings("unchecked")
 	@Override
 	public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
@@ -109,7 +71,6 @@ public class SFDCUsersMerge extends AbstractMessageTransformer {
 			}
 		}
 
-		Collections.sort(mergedUsersList, recordComparator);
 		return mergedUsersList;
 
 	}
