705a2bd8d9f57bc931cf294cd46c65200636eaff
Miroslav Rusin <miroslav.rusin@hotovo.org>
2014-10-22 14:46:42 +0200

    cleanup

--- src/test/java/org/mule/templates/integration/BusinessLogicIT.java
+++ src/test/java/org/mule/templates/integration/BusinessLogicIT.java
@@ -17,22 +17,24 @@ import org.mule.api.MuleEvent;
 import org.mule.processor.chain.SubflowInterceptingChainLifecycleWrapper;
 import org.mule.tck.junit4.rule.DynamicPort;
 
-import com.google.common.collect.Lists;
-
 /**
  * The objective of this class is to validate the correct behavior of the flows for this Mule Template that make calls to external systems.
  * 
  * @author damiansima
  */
 public class BusinessLogicIT extends AbstractTemplateTestCase {
-	private static final String USERS_FROM_ORG_A = "usersFromOrgA";
-	private static final String USERS_FROM_ORG_B = "usersFromOrgB";
-
+	
 	protected static final String TEMPLATE_NAME = "user-aggregation";
 
 	@Rule
 	public DynamicPort port = new DynamicPort("http.port");
 
+	@Test
+	public void testMainFlow() throws Exception {
+		MuleEvent event = runFlow("mainFlow");
+		Assert.assertTrue("The payload should not be null.", "Please find attached your Users Report".equals(event.getMessage().getPayload()));
+	}
+	
 	@Test
 	public void testGatherDataFlow() throws Exception {
 		SubflowInterceptingChainLifecycleWrapper flow = getSubFlow("gatherDataFlow");
@@ -40,24 +42,8 @@ public class BusinessLogicIT extends AbstractTemplateTestCase {
 		flow.initialise();
 		flow.start();
 		MuleEvent event = flow.process(getTestEvent("", MessageExchangePattern.REQUEST_RESPONSE));
-		Iterator<Map<String, String>> list = (Iterator<Map<String, String>>)event.getMessage().getPayload();
-		Assert.assertTrue("There should be users from source A or source B.", Lists.newArrayList(list).size() != 0);
-	}
-
-	@Test
-	public void testFormatOutputFlow() throws Exception {
-		SubflowInterceptingChainLifecycleWrapper flow = getSubFlow("gatherDataFlow");
-		flow.setMuleContext(muleContext);
-		flow.initialise();
-		flow.start();
-		MuleEvent event = flow.process(getTestEvent("", MessageExchangePattern.REQUEST_RESPONSE));
-
-		flow = getSubFlow("formatOutputFlow");
-		flow.setMuleContext(muleContext);
-		flow.initialise();
-		flow.start();
-		event = flow.process(event);
-
-		Assert.assertTrue("The payload should not be null.", event.getMessage().getPayload() != null);
+		Iterator<Map<String, String>> mergedUserList = (Iterator<Map<String, String>>)event.getMessage().getPayload();
+		
+		Assert.assertTrue("There should be users from source A or source B.", mergedUserList.hasNext());
 	}
 }
