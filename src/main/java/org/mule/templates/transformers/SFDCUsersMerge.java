13af28664107ba255b4874529bc97a039b1cd60f
Pablo Mantellini <pmantellini@mulesoft.com>
2015-01-29 14:58:24 -0300

    Updated Readme file

--- src/main/java/org/mule/templates/transformers/SFDCUsersMerge.java
+++ src/main/java/org/mule/templates/transformers/SFDCUsersMerge.java
@@ -8,55 +8,19 @@ package org.mule.templates.transformers;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-<<<<<<< HEAD
 import java.util.List;
 import java.util.Map;
 
-=======
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-
-import org.mule.api.MuleMessage;
-import org.mule.api.transformer.TransformerException;
-import org.mule.transformer.AbstractMessageTransformer;
-
-import com.google.common.collect.Lists;
-
->>>>>>> origin/1.0.1
 /**
  * This transformer will take to list as input and create a third one that will be the merge of the previous two. The identity of an element of the list is
  * defined by its email.
  * 
  * @author cesar.garcia
  */
-<<<<<<< HEAD
 public class SFDCUsersMerge {
 	
 	/**
 	 * The method will merge the users from the two lists creating a new one.
-=======
-public class SFDCUsersMerge extends AbstractMessageTransformer {
-
-	private static final String QUERY_COMPANY_A = "usersFromOrgA";
-	private static final String QUERY_COMPANY_B = "usersFromOrgB";
-
-	@Override
-	public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
-
-		List<Map<String, String>> mergedUsersList = mergeList(getUsersList(message, QUERY_COMPANY_A), getUsersList(message, QUERY_COMPANY_B));
-
-		return mergedUsersList;
-	}
-
-	private List<Map<String, String>> getUsersList(MuleMessage message, String propertyName) {
-		Iterator<Map<String, String>> iterator = message.getInvocationProperty(propertyName);
-		return Lists.newArrayList(iterator);
-	}
-
-	/**
-	 * The method will merge the accounts from the two lists creating a new one.
->>>>>>> origin/1.0.1
 	 * 
 	 * @param usersFromOrgA
 	 *            users from organization A
@@ -64,11 +28,7 @@ public class SFDCUsersMerge extends AbstractMessageTransformer {
 	 *            users from organization B
 	 * @return a list with the merged content of the to input lists
 	 */
-<<<<<<< HEAD
 	public List<Map<String, String>> mergeList(List<Map<String, String>> usersFromOrgA, List<Map<String, String>> usersFromOrgB) {
-=======
-	private List<Map<String, String>> mergeList(List<Map<String, String>> usersFromOrgA, List<Map<String, String>> usersFromOrgB) {
->>>>>>> origin/1.0.1
 		List<Map<String, String>> mergedUsersList = new ArrayList<Map<String, String>>();
 
 		// Put all users from A in the merged mergedUsersList
